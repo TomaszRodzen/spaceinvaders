36e4425a8e93d8d7856e14db02ca1c82e7e394c1
Tomasz Rodzen <t.rodzen@poczta.fm>
2019-10-23 21:39:53 +0200

    setting missile

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# background
+background = pygame.image.load('background.png')
 
 # Title and Icon
 pygame.display.set_caption('Space Invaders')
@@ -23,8 +25,19 @@ playerx_change = 0
 enemyimg= pygame.image.load('enemy1.png')
 enemyx = random.randint(0, 736)
 enemyy = random.randint(64, 150)
-enemyx_change = 0.3
+enemyx_change = 4
 enemyy_change = 40
+
+
+#bullet
+# ready mean you can see bullet
+# file means missile is moving
+missileimg= pygame.image.load('missile1.png')
+missilex = 0
+missiley = 480
+missilex_change = 0
+missiley_change = 10
+missile_state = 'ready'
 
 
 def player(x, y):
@@ -33,6 +46,12 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyimg, (x, y))
+
+
+def fire_missile(x, y):
+    global missile_state
+    missile_state = 'fire'
+    screen.blit(missileimg, (x - 17, y + 10))
 
 
 # game loop. it closes the game
@@ -45,16 +64,21 @@ while running:
         # if key is pressed check if it is right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerx_change = -0.3
+                playerx_change = -5
             if event.key == pygame.K_RIGHT:
-                playerx_change = 0.3
+                playerx_change = 5
+            if event.key == pygame.K_SPACE:
+                if missile_state is 'ready':
+                    missilex = playerx
+                    fire_missile(missilex, missiley)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerx_change = 0
 
     # RGB - Red, Green, Blue from 0 - 255
     screen.fill((13, 0, 0,))
-
+    screen.blit(background, (0, 0))
     # movement in X axis
     playerx += playerx_change
     if playerx <= 0:
@@ -65,11 +89,19 @@ while running:
 
     enemyx += enemyx_change
     if enemyx <= 0:
-        enemyx_change = 0.3
+        enemyx_change = 4
         enemyy += enemyy_change
     elif enemyx >= 736:
-        enemyx_change = -0.3
+        enemyx_change = -4
         enemyy += enemyy_change
+
+    # missile movement
+    if missiley <= 0:
+        missiley = 480
+        missile_state = 'ready'
+    if missile_state is 'fire':
+        fire_missile(missilex, missiley)
+        missiley -= missiley_change
 
     player(playerx, playery)
     enemy(enemyx, enemyy)
